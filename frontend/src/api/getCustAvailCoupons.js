75d76c92bdc116c0837237c3dc10a5c02c818859
b10303097 <b10303097@ntu.edu.tw>
2024-12-07 22:01:16 +0800

    發折價券函數發折價券函數prototype

--- frontend/src/api/getCustAvailCoupons.js
+++ frontend/src/api/getCustAvailCoupons.js
@@ -1,7 +1,6 @@
 // 模組化身份驗證的 API，把 API 呼叫封裝成一般的函式
 import { apiCall } from "../api";
 
-
 export const getCustomerAvailCoupons = async (c_id) => {
   try {
     const response = await apiCall(`/customer/available_coupons`, "POST", {c_id});
